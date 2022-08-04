7c23e25a96391c8a7807dc1d9a2bc3ea30e3c8a8
EnesFaruk1 <enesfaruk08@gmail.com>
2022-08-04 15:28:10 +0300

    Initial commit

---
+++ app/src/main/java/com/enesfaruk/sportfreak/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enesfaruk.sportfreak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
